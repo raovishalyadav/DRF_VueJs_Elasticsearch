e4e63cfb376f637edca806760dc58d68543afca7
Vishal Yadav <110458015+raovishalyadav@users.noreply.github.com>
2022-12-24 10:57:55 +0530

    Update App.vue

--- elasticvue/src/App.vue
+++ elasticvue/src/App.vue
@@ -5,10 +5,10 @@
         <label for="query" class="col-sm-2 col-form-label"><font-awesome-icon icon="fa-solid fa-magnifying-glass" />
           Search:</label>
         <div class="col-sm-10">
-          <input id="query" v-model="query" class="form-control" />
+          <input id="query" v-model="query" @input="debouncedSearch" class="form-control" />
         </div>
       </div>
-      <div class="form-group row my-4"> <!-- added the my-4 class here -->
+      <div class="form-group row my-4">
         <label for="searchType" class="col-sm-2 col-form-label"><font-awesome-icon icon="fa-solid fa-filter" /> Search
           Type:</label>
         <div class="col-sm-10">
@@ -27,17 +27,17 @@
       </div>
     </form>
 
-
     <div v-if="query && submitted">
       <p class="mb-3">{{ total.value }} results found for '{{ query }}'</p>
       <ul class="list-group">
-        <li v-for="(article, index) in highlightedArticles" :key="article.id" class="list-group-item p-2 border border-info">
+        <li v-for="(article, index) in highlightedArticles" :key="article.id"
+          class="list-group-item p-2 border border-info">
           <p class="fw-bold text-decoration-underline">Result {{ index + 1 }}:</p>
           <p>Title:
-            <div v-html="article._source.highlightedTitle"></div>
+          <div v-html="article._source.highlightedTitle"></div>
           </p>
           <p>Content:
-            <div v-html="article._source.highlightedContent"></div>
+          <div v-html="article._source.highlightedContent"></div>
           </p>
         </li>
       </ul>
@@ -45,8 +45,11 @@
   </div>
 </template>
 
+
 <script>
 import axios from 'axios'
+import _ from 'lodash'
+
 export default {
   data() {
     return {
@@ -82,25 +85,29 @@ export default {
         console.error(error)
       }
     },
+    debouncedSearch: _.debounce(function () {
+      this.search()
+    }, 500),
   },
   computed: {
     highlightedArticles() {
       return this.articles.map(article => {
-        let highlightedTitle = article._source.title.replace(new RegExp(this.query, 'gi'), `<span class="bg-info fw-bold">${this.query}</span>`);
-        let highlightedContent = article._source.content.replace(new RegExp(this.query, 'gi'), `<span class="bg-info fw-bold">${this.query}</span>`);
+        let highlightedTitle = article._source.title.replace(new RegExp(this.query, 'gi'), `<span class="bg-info text-white">${this.query}</span>`)
+        let highlightedContent = article._source.content.replace(new RegExp(this.query, 'gi'), `<span class="bg-info text-white">${this.query}</span>`)
         return {
           ...article,
           _source: {
             ...article._source,
             highlightedTitle,
             highlightedContent,
-          }
+          },
         }
       })
-    }
-  }
+    },
+  },
 }
 </script>
+
 
 <style>
 
